4c0c536b23f0e2b01db7af0a3f4293aba7096e77
Juancaceres963 <dnijuancaceres@gmail.com>
2023-02-28 17:37:29 -0300

    Carrusel de imagenes de presentacion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
-    var prueba = "Soy una variable de jquery";
-    console.log(prueba);
+    /* Carrusel */
+    var carousel = document.querySelector('.carousel');
+var prevButton = carousel.querySelector('.carousel-prev');
+var nextButton = carousel.querySelector('.carousel-next');
+var images = carousel.querySelectorAll('img');
+var imageIndex = 0;
+
+setInterval(function() {
+  imageIndex++;
+  if (imageIndex >= images.length) {
+    imageIndex = 0;
+  }
+  carousel.querySelector('ul').style.transform = 'translateX(-' + (imageIndex * 33.333) + '%)';
+}, 30000);
+
+prevButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  imageIndex--;
+  if (imageIndex < 0) {
+    imageIndex = images.length - 1;
+  }
+  carousel.querySelector('ul').style.transform = 'translateX(-' + (imageIndex * 33.333) + '%)';
+});
+
+nextButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  imageIndex++;
+  if (imageIndex >= images.length) {
+    imageIndex = 0;
+  }
+  carousel.querySelector('ul').style.transform = 'translateX(-' + (imageIndex * 33.333) + '%)';
+});
+``
+
 });
